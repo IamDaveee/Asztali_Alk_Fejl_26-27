ad8c6da5a90e49750fcd7d197211ade76c73ffb4
Dave <121976848+IamDaveee@users.noreply.github.com>
2026-09-07 14:04:16 +0200

    Add files via upload

---
+++ VizibicikliKolcsonzo/Kolcsonzes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizibicikliKolcsonzo
+{
+    internal class Kolcsonzes
+    {
+        public string nev, jazon;
+        public int elvitelOra, elvitelPerc, visszaOra, visszaPerc;
+        public int elvOraPerc, visszaOraPerc;
+
+        public Kolcsonzes(string nev, string jazon, int elvitelOra, int elvitelPerc, int visszaOra, int visszaPerc)
+        {
+            this.nev = nev;
+            this.jazon = jazon;
+            this.elvitelOra = elvitelOra;
+            this.elvitelPerc = elvitelPerc;
+            this.visszaOra = visszaOra;
+            this.visszaPerc = visszaPerc;
+        }
+
+        public Kolcsonzes(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            nev= adatok[0];
+            jazon= adatok[1];
+            elvitelOra= Convert.ToInt32(adatok[2]);
+            elvitelPerc= Convert.ToInt32(adatok[3]);
+            visszaOra= Convert.ToInt32(adatok[4]);
+            visszaPerc = Convert.ToInt32(adatok[5]);
+            elvOraPerc = elvitelOra * 60 + elvitelPerc;
+            visszaOraPerc = visszaOra * 60 + visszaPerc;
+        }
+
+        //függvény átalakításra
+        public int Percben(int ora,int perc)
+        {
+            return ora * 60 + perc;
+        }
+    }
+}
---
+++ VizibicikliKolcsonzo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VizibicikliKolcsonzo
+{
+    internal class Program
+    {
+        static List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
+        static void Main(string[] args)
+        {
+            File.ReadAllLines("kolcsonzesek.txt").Skip(1).ToList().ForEach(x => kolcsonzesek.Add(new Kolcsonzes(x)));
+
+            foreach (var item in kolcsonzesek)
+            {
+                Console.WriteLine($"{item.nev};{item.jazon};{item.elvitelOra};{item.elvitelPerc};{item.visszaOra};{item.visszaPerc}");
+            }
+            Console.WriteLine(kolcsonzesek.Count);
+
+            Console.WriteLine("Melyik személyt keressük?");
+            string nev = Console.ReadLine().ToString();
+
+            Console.WriteLine();
+            
+            if (kolcsonzesek.Any(x=>x.nev==nev))
+            {
+                Console.WriteLine("Kölcsönzései:");
+                kolcsonzesek.Where(x => x.nev == nev).ToList().ForEach(x => Console.WriteLine($"{x.elvitelOra:00}:{x.elvitelPerc:00}-{x.visszaOra:00}:{x.visszaPerc:00}"));
+            }
+            else
+            {
+                Console.WriteLine($"A keresett személynek nem volt aznap kölcsönzése({nev})");
+            }
+
+
+            Console.WriteLine("Adjon meg egy időpontot óra:perc alakban:");
+            string[] oraPerc = Console.ReadLine().Split(':');
+            int ora = Convert.ToInt32(oraPerc[0]);
+            int perc = Convert.ToInt32(oraPerc[1]);
+            int Percben = ora * 60 + perc;
+            kolcsonzesek.Where(x => x.elvOraPerc<=Percben && x.visszaOraPerc>=Percben).ToList().ForEach(x=> Console.WriteLine($"{x.elvitelOra:00}:{x.elvitelPerc:00}-{x.visszaOra:00}:{x.visszaPerc:00} - {x.nev}"));
+
+            double osszeg = kolcsonzesek.Sum(x=>Math.Ceiling((x.visszaOraPerc-x.elvOraPerc)/30.0)*2400);
+            Console.WriteLine($"Napi bevétel: {osszeg:C0}"); //C0: ezres csoportosításés pénznem (currency)
+
+            StreamWriter sw = new StreamWriter("F.text");
+            kolcsonzesek.Where(x => x.jazon == "F").ToList().ForEach(x=>sw.WriteLine($"{x.elvitelOra:00}:{x.elvitelPerc:00}-{x.visszaOra:00}:{x.visszaPerc:00} - {x.nev}"));
+            sw.Close();
+
+            Console.WriteLine("Statisztika:");
+            kolcsonzesek.OrderBy(x => x.jazon).GroupBy(x => x.jazon).ToList().ForEach(x => Console.WriteLine($"{x.Key} - {x.Count()}"));
+        }
+        
+    }
+}
